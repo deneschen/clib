36b3de331f5f19d99bb49371be0354bf21233082
deneschen <deneschen@foxmail.com>
2023-10-06 23:22:25 -0700

    arith

---
+++ include/arith.h
@@ -0,0 +1,11 @@
+#ifndef __ARITH_H
+#define __ARITH_H
+
+int arith_max(int x, int y);
+int arith_min(int x, int y);
+int arith_div(int x, int y);
+int arith_mod(int x, int y);
+int arith_floor(int x, int y);
+int arith_ceiling(int x, int y);
+
+#endif /* __ARITH_H */
--- scripts/cmake_helper.cmake
+++ scripts/cmake_helper.cmake
@@ -25,3 +25,29 @@ macro(add_exec)
         PUBLIC
         ${ARG_EXT_LDLIBS})
 endmacro()
+
+macro(add_lib)
+    set(opts SHARED_LIB)
+    set(one_val_args)
+    set(multi_val_args SRCS EXT_CCFLAGS EXT_LDFLAGS EXT_LDLIBS)
+    cmake_parse_arguments(ARG "${opts}" "${one_val_args}" "${multi_val_args}" ${ARGN})
+
+    if(${ARG_SHARED_LIB})
+        add_library(${ARG_UNPARSED_ARGUMENTS} SHARED)
+    else()
+        add_library(${ARG_UNPARSED_ARGUMENTS} STATIC)
+    endif()
+
+    target_sources(${ARG_UNPARSED_ARGUMENTS}
+        PUBLIC
+        ${ARG_SRCS})
+    target_compile_options(${ARG_UNPARSED_ARGUMENTS}
+        PUBLIC
+        ${ARG_EXT_CCFLAGS})
+    target_link_options(${ARG_UNPARSED_ARGUMENTS}
+        PUBLIC
+        ${ARG_EXT_LDFLAGS})
+    target_link_libraries(${ARG_UNPARSED_ARGUMENTS}
+        PUBLIC
+        ${ARG_EXT_LDLIBS})
+endmacro()
